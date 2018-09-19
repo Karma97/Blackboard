1b12a4e03fa2b974e1d7394bfe3a8941b4f27de7
nic-schi <nic-schi@users.noreply.github.com>
2018-09-19 22:15:20 +0200

    PDO array UTF-8

--- index.php
+++ index.php
@@ -24,8 +24,9 @@ try {
 	$dbpass = 'pimmelfresse';
 	$dbhost = 'vweb19.nitrado.net';
 	$dbname='ni143121_5sql1';
-	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
+
 catch (PDOException $e) {
 echo "Error : " . $e->getMessage() . "<br/>";
 die("<br><font color='red'>ERROR - Es konnte keine Verbindung zu der Datenbank aufgebaut werden.</font>");
