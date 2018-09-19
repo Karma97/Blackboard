b955987cfc3376bc52c450377fa311d75eba4852
nic-schi <nic-schi@users.noreply.github.com>
2018-09-19 23:20:19 +0200

    fixed the error

--- anzeigelesen.php
+++ anzeigelesen.php
@@ -26,20 +26,21 @@ try {
 }
 
 catch (PDOException $e) {
-echo "Error : " . $e->getMessage() . "<br/>";
-die("<br><font color='red'>ERROR - Es konnte keine Verbindung zu der Datenbank aufgebaut werden.</font>");
+	echo "Error : " . $e->getMessage() . "<br/>";
+	die("<br><font color='red'>ERROR - Es konnte keine Verbindung zu der Datenbank aufgebaut werden.</font>");
 }
 
 $sql = "SELECT * FROM 'anzeige' INNER JOIN inserent USING (Inserentennr) INNER JOIN besitzt USING (Anzeigennr) WHERE Rubriknr = '$zahl'";
 
 foreach ($dbh -> query($sql) as $row) {
-	
 	echo "<table id='table1'><tr><td>Anzeigendatum:</td><td>".$row['Anzeigendatum']."</td></tr>
 	<tr><td>Nickname:</td><td>".$row['Nickname']."</td></tr>
 	<tr><td>E-Mail:</td><td>".$row['Email']."</td></tr>
 	<tr><td>Anzeigentext:</td><td>".$row['Anzeigentext']."</td></tr></table><br>";
 	
 }
+
+$dbh -> null;
 ?>
 <br><br><br><br><br>
 <form action="anzeigeneu.php"> <input type="submit" value="Neue Anzeige hinzufügen"/></form><br><br><br>
