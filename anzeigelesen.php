e38ca835cfb41e21bfc97dc506480367a529f347
nic-schi <nic-schi@users.noreply.github.com>
2018-09-19 23:12:29 +0200

    test

--- anzeigelesen.php
+++ anzeigelesen.php
@@ -36,7 +36,7 @@ foreach ($dbh -> query($sql) as $row) {
 	
 	echo "<table id='table1'><tr><td>Anzeigendatum:</td><td>".$row['Anzeigendatum']."</td></tr>
 	<tr><td>Nickname:</td><td>".$row['Nickname']."</td></tr>
-	<tr><td>E-Mail:</td><td>".$row['Email']."</td></tr>
+	<tr><td>E-Mail3:</td><td>".$row['Email']."</td></tr>
 	<tr><td>Anzeigentext:</td><td>".$row['Anzeigentext']."</td></tr></table><br>";
 	
 }
