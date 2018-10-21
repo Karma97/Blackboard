394317b252dbc039c1243658234b9a2ae1fbabec
nic-schi <nic-schi@users.noreply.github.com>
2018-10-21 21:11:00 +0200

    3

--- js/searchNews.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-	$("#searchN").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#alle_news div.card").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
